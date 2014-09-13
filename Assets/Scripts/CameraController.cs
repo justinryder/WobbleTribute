9d457b2e6106b0adbc2be39b2247fb9d1bd95c73
Justin Ryder <justin@justin-ryder.com>
2014-09-12 22:18:15 -0400

    WOBBLE COLORZZZZ and beginning of camera controller.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+  public GameObject Target;
+
+  public Vector3 DistanceFromTarget;
+
+  public void Update()
+  {
+    transform.position = Target.transform.TransformDirection(DistanceFromTarget);
+    transform.LookAt(Target.transform);
+  }
+}
