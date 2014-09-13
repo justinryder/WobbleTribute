11b1ca017d5f37aafd5dadc52231c64d4cc29db2
Justin Ryder <justin@justin-ryder.com>
2014-09-13 16:24:01 -0400

    added menu scene. DON'T RENAME ANY SCENES OR IT'S GONNA BREAK!!!!!

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+  public KeyCode QuitToMenuKey = KeyCode.Escape;
+
+  public void Start()
+  {
+    DontDestroyOnLoad(gameObject);
+  }
+
+  public void OnGUI()
+  {
+    if (Application.loadedLevelName == "menu")
+    {
+      if (GUILayout.Button("Kanye Target"))
+      {
+        Application.LoadLevel("kanyetarget");
+      }
+      if (GUILayout.Button("Test"))
+      {
+        Application.LoadLevel("test");
+      }
+    }
+    else
+    {
+      if (Input.GetKeyUp(QuitToMenuKey))
+      {
+        Application.LoadLevel("menu");
+      }
+    }
+  }
+}
--- Assets/Scripts/WobbleController.cs
+++ Assets/Scripts/WobbleController.cs
@@ -1,63 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
-public class WobbleController : MonoBehaviour 
+public class WobbleController : MonoBehaviour
 {
-	#region public variables
-	public string AxisName;
-	public float Speed;
-	public GameObject RightPaw,
-					  LeftPaw;
-	#endregion
+  #region public variables
 
-	#region private variables
-	private Vector3 _rightLocalPosition,
-					_leftLocalPosition;
-	#endregion
+  public string AxisName;
 
-	void Awake()
-	{
-		_rightLocalPosition = RightPaw.transform.localPosition;
-	  _leftLocalPosition = LeftPaw.transform.localPosition;
-	}
+  public float Speed;
 
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		var input = Input.GetAxis(AxisName + "Vertical");
+  public GameObject RightPaw, LeftPaw;
 
-		if (input > 0)
-		{
-			RightPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);
-			LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);
-		}
-		else if (input < 0)
-		{
-			RightPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
-			LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
-		}
+  #endregion
 
-		input = Input.GetAxis(AxisName + "Horizontal");
+  private void Update()
+  {
+    var input = Input.GetAxis(AxisName + "Vertical");
 
-		if (input > 0)
-		{
-			RightPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
-			LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);			
-		}
-		else if (input < 0)
-		{
-			RightPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);
-			LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
-		}
+    if (input > 0)
+    {
+      RightPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);
+      LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);
+    }
+    else if (input < 0)
+    {
+      RightPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
+      LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
+    }
 
-		/*RightPaw.transform.localPosition = _rightLocalPosition;
-		LeftPaw.transform.localPosition = _leftLocalPosition;*/
-	}
+    input = Input.GetAxis(AxisName + "Horizontal");
+
+    if (input > 0)
+    {
+      RightPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
+      LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);
+    }
+    else if (input < 0)
+    {
+      RightPaw.rigidbody.AddRelativeTorque(Vector3.right * Speed);
+      LeftPaw.rigidbody.AddRelativeTorque(Vector3.right * -Speed);
+    }
+
+    rigidbody.AddRelativeForce(Vector3.right * input);
+  }
 }
