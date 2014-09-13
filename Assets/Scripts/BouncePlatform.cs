d902b6a112c8df0b3dad64e37cdbf8c953b7ad6a
Justin Ryder <justin@justin-ryder.com>
2014-09-13 15:37:33 -0400

    naskjdgnkadsnk

---
+++ Assets/Scripts/BouncePlatform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncePlatform : MonoBehaviour
+{
+  public Vector3 BounceForce = new Vector3(0, 10, 0);
+
+  public void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Player")
+    {
+      collision.rigidbody.AddForce(BounceForce, ForceMode.Impulse);
+    }
+  }
+}
---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+  public SpawnController SpawnPoint;
+
+  public void OnTriggerEnter(Collider collider)
+  {
+    if (collider.tag == "Player")
+    {
+      Destroy(collider.gameObject);
+      SpawnPoint.SpawnWobbleDog();
+    }
+  }
+}
