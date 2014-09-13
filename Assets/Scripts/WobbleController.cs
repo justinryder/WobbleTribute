8ed1de92c02bad111c561d75aadc5f26b0feb690
Rob Drury <robdrury@stu-68-149-171-184.champlain.edu>
2014-09-12 20:50:24 -0400

    fffffff

---
+++ Assets/Scripts/WobbleController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WobbleController : MonoBehaviour 
+{
+	#region public variables
+	public string AxisName;
+	public float speed;
+	#endregion
+
+	#region private variables
+	private List<GameObject> feet;
+	#endregion
+
+	void Awake()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
