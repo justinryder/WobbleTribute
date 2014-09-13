1eb21389c2dc1df9149d58222cb9cba0cff79dd4
Rob Drury <robdrury@stu-68-149-171-184.champlain.edu>
2014-09-13 12:20:24 -0400

    moar wobbles

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour 
+{
+	#region public variables
+	public Vector3 MovementOffset;
+	public float duration;
+	#endregion
+
+	#region private variables
+	private Vector3 _initPos;
+	#endregion
+
+	void Awake()
+	{
+		_initPos = transform.position;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		var sinVal = Mathf.Sin(Time.time / duration);
+		var tmpTranslation = (MovementOffset * sinVal);
+
+		transform.position = _initPos + tmpTranslation;
+	}
+}
