63ef0c92677000087261b99b69709e34abbf353e
Justin Ryder <justin@justin-ryder.com>
2014-09-12 23:08:03 -0400

    added camera wobbles!

---
+++ Assets/Scripts/Vector3Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Vector3Extensions
+{
+  public static Vector3 Clamp(this Vector3 value, Vector3 min, Vector3 max)
+  {
+    return new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+  }
+}
