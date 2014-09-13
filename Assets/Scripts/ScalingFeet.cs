8d8cd9fa44ef5083719b15c1854f65b203c49725
Rob Drury <robdrury@stu-68-149-171-184.champlain.edu>
2014-09-13 13:20:25 -0400

    some stuff

---
+++ Assets/Scripts/ScalingFeet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalingFeet : MonoBehaviour 
+{
+	#region public variables
+	public float ScaleOffset;
+	#endregion
+
+	#region private variables
+	private Vector3 _initScale,
+					_currentScale;
+	#endregion
+
+	void Awake ()
+	{
+		_initScale = transform.localScale;
+		_currentScale = Vector3.zero;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var tmpSinScale = ScaleOffset * Mathf.Sin(Time.time);
+		var tmpCosScale = ScaleOffset * Mathf.Cos(Time.time);
+		tmpSinScale += tmpSinScale;
+		tmpCosScale += tmpCosScale;
+		_currentScale.y = _initScale.y * tmpSinScale;
+		_currentScale.z = _initScale.z * tmpCosScale;
+		transform.localScale = _initScale + _currentScale;
+	}
+}
