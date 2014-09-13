5e13ca7e386645aef30cfda3fe9a11a3121d3b10
Rob Drury <robdrury@stu-68-149-171-184.champlain.edu>
2014-09-12 23:17:46 -0400

    justin shut up justin

--- Assets/Scripts/WobbleController.cs
+++ Assets/Scripts/WobbleController.cs
@@ -6,16 +6,20 @@ public class WobbleController : MonoBehaviour
 {
 	#region public variables
 	public string AxisName;
-	public float speed;
+	public float Speed;
+	public GameObject RightPaw,
+					  LeftPaw;
 	#endregion
 
 	#region private variables
-	private List<GameObject> feet;
+	private Vector3 _rightLocalPosition,
+					_leftLocalPosition;
 	#endregion
 
 	void Awake()
 	{
-		
+		_rightLocalPosition = RightPaw.transform.localPosition;
+		_leftLocalPosition = LeftPaw.transform.localPosition;
 	}
 
 	// Use this for initialization
@@ -27,6 +31,33 @@ public class WobbleController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		var input = Input.GetAxis(AxisName + "Vertical");
 
+		if (input > 0)
+		{
+			RightPaw.rigidbody.AddTorque(RightPaw.transform.right * Speed);
+			LeftPaw.rigidbody.AddTorque(LeftPaw.transform.right * Speed);
+		}
+		else if (input < 0)
+		{
+			RightPaw.rigidbody.AddTorque(RightPaw.transform.right * -Speed);
+			LeftPaw.rigidbody.AddTorque(LeftPaw.transform.right * -Speed);
+		}
+
+		input = Input.GetAxis(AxisName + "Horizontal");
+
+		if (input > 0)
+		{
+			RightPaw.rigidbody.AddTorque(RightPaw.transform.right * -Speed);
+			LeftPaw.rigidbody.AddTorque(LeftPaw.transform.right * Speed);			
+		}
+		else if (input < 0)
+		{
+			RightPaw.rigidbody.AddTorque(RightPaw.transform.right * Speed);
+			LeftPaw.rigidbody.AddTorque(LeftPaw.transform.right * -Speed);
+		}
+
+		/*RightPaw.transform.localPosition = _rightLocalPosition;
+		LeftPaw.transform.localPosition = _leftLocalPosition;*/
 	}
 }
