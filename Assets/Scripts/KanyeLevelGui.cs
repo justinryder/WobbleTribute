8c7d8f38886bc8ef4e949be06acaecfb9ca3e023
Justin Ryder <justin@justin-ryder.com>
2014-09-13 22:05:39 -0400

    added toggle keys for in game GUI areas.

--- Assets/Scripts/KanyeLevelGui.cs
+++ Assets/Scripts/KanyeLevelGui.cs
@@ -12,6 +12,17 @@ public class KanyeLevelGui : MonoBehaviour
 
   public int LineWidth = 200;
 
+  public bool ShowControls = true;
+
+  public bool ShowInstructions = true;
+
+  public bool ShowScore = true;
+  
+  public KeyCode ToggleAllKey = KeyCode.Alpha1;
+  public KeyCode ToggleControlsKey = KeyCode.Alpha2;
+  public KeyCode ToggleInstructionsKey = KeyCode.Alpha3;
+  public KeyCode ToggleScoreKey = KeyCode.Alpha4;
+
   private readonly string[] _instructions =
   {
     "Don't fall into the lava.",
@@ -28,8 +39,35 @@ public class KanyeLevelGui : MonoBehaviour
     "Space - Jump",
     "Backspace - Suicide",
     "C - Toggle New/Classic Camera",
-    "X - Toggle Kanye Camera"
+    "X - Toggle Kanye Camera",
+    "1 - Toggle Full GUI",
+    "2 - Toggle Controls",
+    "3 - Toggle Instructions",
+    "4 - Toggle Score"
   };
+
+  public void Update()
+  {
+    if (Input.GetKeyUp(ToggleAllKey))
+    {
+      ShowControls = ShowInstructions = ShowScore = !(ShowControls || ShowInstructions || ShowScore);
+    }
+
+    if (Input.GetKeyUp(ToggleControlsKey))
+    {
+      ShowControls = !ShowControls;
+    }
+
+    if (Input.GetKeyUp(ToggleInstructionsKey))
+    {
+      ShowInstructions = !ShowInstructions;
+    }
+
+    if (Input.GetKeyUp(ToggleScoreKey))
+    {
+      ShowScore = !ShowScore;
+    }
+  }
 
   public void OnGUI()
   {
@@ -40,38 +78,47 @@ public class KanyeLevelGui : MonoBehaviour
 
   private void DrawInstructions()
   {
-    var width = LineWidth;
-    var height = LineHeight;
-    var x = Screen.width - width - Margin;
-    var y = Screen.height - height - Margin;
-    foreach (var instruction in _instructions)
+    if (ShowInstructions)
     {
-      GUI.Label(new Rect(x, y, width, height), instruction);
-      y -= height;
+      var width = LineWidth;
+      var height = LineHeight;
+      var x = Screen.width - width - Margin;
+      var y = Screen.height - height - Margin;
+      foreach (var instruction in _instructions)
+      {
+        GUI.Label(new Rect(x, y, width, height), instruction);
+        y -= height;
+      }
     }
   }
 
   private void DrawControls()
   {
-    var width = LineWidth;
-    var height = LineHeight;
-    var x = Margin;
-    var y = Margin;
-    foreach (var control in _controls)
+    if (ShowControls)
     {
-      GUI.Label(new Rect(x, y, width, height), control);
-      y += height;
+      var width = LineWidth;
+      var height = LineHeight;
+      var x = Margin;
+      var y = Margin;
+      foreach (var control in _controls)
+      {
+        GUI.Label(new Rect(x, y, width, height), control);
+        y += height;
+      }
     }
   }
 
   private void DrawScore()
   {
-    var width = LineWidth;
-    var height = LineHeight;
-    var x = Margin;
-    var y = Screen.height - height - Margin;
-    GUI.Label(new Rect(x, y, width, height), "Deaths: " + Score.Deaths);
-    y -= height;
-    GUI.Label(new Rect(x, y, width, height), "Points: " + Score.Points);
+    if (ShowScore)
+    {
+      var width = LineWidth;
+      var height = LineHeight;
+      var x = Margin;
+      var y = Screen.height - height - Margin;
+      GUI.Label(new Rect(x, y, width, height), "Deaths: " + Score.Deaths);
+      y -= height;
+      GUI.Label(new Rect(x, y, width, height), "Points: " + Score.Points);
+    }
   }
 }
